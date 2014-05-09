d91cd58b8b5389d366fc0fc6e22f82e434879aa6
Angus Mark <ngsmrk@gmail.com>
2014-05-09 14:03:37 +0100

    Console message.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,5 @@
 Pebble.addEventListener("ready",
     function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
+        console.log("Thesaurus Rex is ready!!!");
     }
 );
