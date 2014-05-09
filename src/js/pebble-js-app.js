a927a3aab992c2533f1554788ead1fa2760edf5e
Angus Mark <ngsmrk@gmail.com>
2014-05-09 14:13:52 +0100

    Works but doesn't display.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,32 @@
 Pebble.addEventListener("ready",
-    function(e) {
+    function (e) {
         console.log("Thesaurus Rex is ready!!!");
+        fetch_random_word();
     }
 );
+
+function fetch_random_word() {
+
+    var req = new XMLHttpRequest();
+    req.open('GET', 'http://thesaurus-rex.herokuapp.com/random_word/', true);
+    req.onload = function (e) {
+        var status = req.status;
+        var readyState = req.readyState;
+
+        console.log("Status: " + status + ", state: " + readyState);
+        if (req.readyState == 4 && req.status == 200) {
+            responseText = req.responseText;
+            console.log(responseText);
+
+            var response = JSON.parse(req.responseText);
+
+            var shippingDate = response.word;
+            console.log(shippingDate);
+
+        } else {
+            console.log("Error");
+        }
+    };
+
+    req.send(null);
+}
