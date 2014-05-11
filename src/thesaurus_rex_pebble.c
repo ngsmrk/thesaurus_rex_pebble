2f5e5b2805699e209f3745295685adcae7187603
Angus Mark <ngsmrk@gmail.com>
2014-05-11 11:24:41 -0700

    Removed up and down button handlers

--- src/thesaurus_rex_pebble.c
+++ src/thesaurus_rex_pebble.c
@@ -69,8 +69,6 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
 static void app_message_init(void) {
